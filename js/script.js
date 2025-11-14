0800554f0d90e06826a5de929f8bdc8d3bddc29c
Vanja7skr <141807701+Vanja7skr@users.noreply.github.com>
2025-11-13 22:29:05 -0600

    task is done

---
+++ js/script.js
@@ -0,0 +1,41 @@
+// 1
+const age = "13";
+console.log(age);
+
+// 2
+const name = "Ivan";
+console.log(name);
+
+// 3
+const isStudent = "True";
+console.log(isStudent);
+
+// 4
+const myString = "It's only after we've lost everything that we're free to do anything";
+console.log(myString);
+
+// 5
+let myNumber = 9;
+console.log(myNumber + 10);
+
+// 6
+const myNull = null;
+console.log(myNull);
+
+// 7
+const userName = "Введіть своє ім’я"
+const promptAnswer =  prompt(userName)
+console.log(promptAnswer)
+alert(promptAnswer)
+console.log(typeof promptAnswer)
+
+// 8
+const promptIn = "Ви дійсно хочете покинути сторінку?"
+const promptOut =  confirm(promptIn)
+console.log(promptOut)
+
+// 9
+const alertIn = "Увага! Дія може бути небезпечною."
+const alertOut =  alert(alertIn)
+const confirmIn = "Ви впевнені, що хочете продовжити?"
+const confirmOut = confirm(confirmIn)
